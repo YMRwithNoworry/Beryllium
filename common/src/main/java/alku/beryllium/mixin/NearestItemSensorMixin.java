8b2f9a6655d82b1d370e9c039e3e1424a269bf32
YMRwithNoworry <YMRwithNoworry@users.noreply.github.com>
2026-07-02 12:47:56 +0800

    Preserve nearest item predicate order

--- common/src/main/java/alku/beryllium/mixin/NearestItemSensorMixin.java
+++ common/src/main/java/alku/beryllium/mixin/NearestItemSensorMixin.java
@@ -27,17 +27,15 @@ public class NearestItemSensorMixin {
             mob.getBoundingBox().inflate(32.0, 16.0, 32.0),
             item -> true
         );
-        Optional<ItemEntity> nearestWantedItem = EntityDistanceSort.findFirstWithinExclusiveDistanceSortedByDistance(
-            items,
-            mob.getX(),
-            mob.getY(),
-            mob.getZ(),
-            32.0,
-            ItemEntity::getX,
-            ItemEntity::getY,
-            ItemEntity::getZ,
-            item -> mob.wantsToPickUp(item.getItem()) && mob.hasLineOfSight(item)
-        );
+        EntityDistanceSort.sortByDistance(items, mob);
+
+        Optional<ItemEntity> nearestWantedItem = Optional.empty();
+        for (ItemEntity item : items) {
+            if (mob.wantsToPickUp(item.getItem()) && item.closerThan(mob, 32.0) && mob.hasLineOfSight(item)) {
+                nearestWantedItem = Optional.of(item);
+                break;
+            }
+        }
         brain.setMemory(MemoryModuleType.NEAREST_VISIBLE_WANTED_ITEM, nearestWantedItem);
     }
 }
